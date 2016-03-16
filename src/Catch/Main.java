228f254f698cf2c39893fa86064cdbbb0f56a73c
Evgenyi Slavnyi <Swam@MacBook-Pro-Evgenyi.local>
2016-03-16 00:20:13 -0700

    ok

--- src/Catch/Main.java
+++ src/Catch/Main.java
@@ -1,7 +1,22 @@
 package Catch;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Created by Swam on 15.03.16.
  */
-public class Main {
+public class Main  {
+    public static void main(String[] args) throws IOException {
+        Generator generator = new Generator();
+        Reader rd = new Reader();
+        System.out.println("Введите количество символов в пароле: ");
+        int tmp = rd.read();
+        generator.genPass = new String[tmp];
+        System.out.print("Ваш пароль: ");
+        generator.passGen();
+        try (FileWriter writer = new FileWriter("")){
+
+        }
+    }
 }
--- src/Catch/Reader.java
+++ src/Catch/Reader.java
@@ -1,7 +1,19 @@
 package Catch;
 
+import java.io.BufferedReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by Swam on 15.03.16.
  */
 public class Reader {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    int read() throws IOException {
+        String x = reader.readLine();
+        int z = Integer.parseInt(x);
+        return z;
+    }
 }
